00ceac592caa1d7c5e4a7d7cb43689c706288bcd
rafald1 <116283372+rafald1@users.noreply.github.com>
2024-02-23 10:41:30 +0000

    Pending changes exported from your codespace

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -13,19 +13,32 @@ Follow the instructions below to get started:
 
 from collections import namedtuple
 
+MAX_ITEM_QUANTITY = 1000
+MAX_ITEM_AMOUNT = 1_000_000
+MAX_ALL_ITEMS_AMOUNT = 1_000_000
+
+
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
 def validorder(order: Order):
+    all_items_amount = 0
     net = 0
 
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            if abs(item.amount) < MAX_ITEM_AMOUNT:
+                all_items_amount = round(all_items_amount, 2) + round(item.amount, 2)
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            if item.amount < MAX_ITEM_AMOUNT and item.quantity < MAX_ITEM_QUANTITY:
+                net = round(net, 2) - round(item.amount, 2) * item.quantity
         else:
             return "Invalid item type: %s" % item.type
+        
+        if all_items_amount > MAX_ALL_ITEMS_AMOUNT:
+            return 'Total amount payable for an order exceeded'
+
+    net += round(all_items_amount, 2)
 
     if net != 0:
         return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
